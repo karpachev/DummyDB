bde305a3a77abec2fa2976d6fe122bdf2e70cd7c
Vasil Karpachev <v.karpachev@gmail.com>
2015-04-17 17:09:36 +0300

    improved the tests

--- src/tablet/memtable.h
+++ src/tablet/memtable.h
@@ -1,36 +1,18 @@
 #ifndef MEMTABLE_H
 #define MEMTABLE_H
-#include "base/key.h"
-#include "base/value.h"
-#include "base/result.h"
-#include <vector>
+#include "base/sorted_block.h"
 namespace DummyDB
 {
 
-class MemTable
+class MemTable : public SortedBlock
 {
     public:
         MemTable();
         virtual ~MemTable();
 
-        Result<Value>   set(const Key& key, const Value& value);
-        Result<Value>   remove(const Key&);
-
-        Result<Value>   get(const Key& ) const;
-        Result< std::vector<Value> >
-                        prefixSearch(const Key& ) const;
-
-        unsigned int    numbElements() { return mem_table.size(); }
-
-        /// Printout the table to the stdout.. Mostly for debuggin
-        void            toStdout();
-
     protected:
-        /// Find the index of Key, or -1 otherwise
-        int             indexOf(const Key& ) const;
     private:
-        std::vector< std::pair<Key,Value> >
-                        mem_table;
+
 };
 
 } // namespace DummyDB
